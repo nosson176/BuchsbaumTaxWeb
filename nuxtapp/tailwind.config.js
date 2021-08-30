8fbafcd8758ef7d0f9074b89ada661aae5c729e2
Yoshi Rubin <yoshrubin@gmail.com>
2021-08-30 16:20:50 +0300

    installed font for tailwind

--- nuxtapp/tailwind.config.js
+++ nuxtapp/tailwind.config.js
@@ -4,11 +4,17 @@
 ** Docs: https://tailwindcss.com/docs/configuration
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   theme: {
     extend: {
       screens: {
         print: { raw: 'print' }
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
       }
     }
   }
