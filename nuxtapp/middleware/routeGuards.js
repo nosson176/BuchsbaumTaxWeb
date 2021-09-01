dfc07a716104d9b7e0c4d02d8ef296740dc87181
Yoshi Rubin <yoshrubin@gmail.com>
2021-09-01 16:03:29 +0300

    added authGuard to routes

---
+++ nuxtapp/middleware/routeGuards.js
@@ -0,0 +1,19 @@
+import {
+  ROUTE_LOGIN,
+  GET_DOES_USER_HAVE_SESSION
+} from '@/shared/constants'
+
+const unguardedRoutesHash = {
+  [ROUTE_LOGIN]: true
+}
+
+export default function ({ route, redirect, store }) {
+  const isAuthenticated = store.getters[GET_DOES_USER_HAVE_SESSION]
+  if (route.name) {
+    if (!isAuthenticated) {
+      if (!unguardedRoutesHash[route.name]) {
+        redirect({ name: ROUTE_LOGIN })
+      }
+    }
+  }
+}
