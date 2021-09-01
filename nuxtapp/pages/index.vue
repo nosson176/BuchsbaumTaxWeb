2e16592276da2177e23ee7c8235026b9f6f634f3
Yoshi Rubin <yoshrubin@gmail.com>
2021-09-01 16:36:22 +0300

    redirect home

--- nuxtapp/pages/index.vue
+++ nuxtapp/pages/index.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="my-0 mx-auto min-h-screen">
-    <div class="flex justify-center pt-8">
-      <ExampleForm />
-    </div>
-  </div>
+  <div />
 </template>
 
 <script>
+import { ROUTE_HOME } from '~/shared/constants'
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  middleware ({ redirect }) {
+    return redirect({ name: ROUTE_HOME })
+  }
 }
 </script>
