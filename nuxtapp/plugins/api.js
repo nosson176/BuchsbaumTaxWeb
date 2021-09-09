0eb982dcf5e7913963dc9721200cfa5ccb6aa76b
Yoshi Rubin <yoshrubin@gmail.com>
2021-09-09 17:25:23 +0300

    cleaned some comments

--- nuxtapp/plugins/api.js
+++ nuxtapp/plugins/api.js
@@ -3,11 +3,9 @@ import { models } from '~/shared/constants'
 export default function ({ $axios }, inject) {
   const login = () => $axios.get('/api/sessions', { loading: models.token })
 
-  // Create a custom axios instance
   const api = {
     login
   }
-
   // Inject to context as $api
   inject('api', api)
 }
