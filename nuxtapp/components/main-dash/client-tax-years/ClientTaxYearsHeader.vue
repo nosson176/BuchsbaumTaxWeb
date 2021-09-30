028e13a5295e408b5a4e11ed706f92c1d80a88ec
Yoshi Rubin <yoshrubin@gmail.com>
2021-09-30 12:42:56 +0300

    showing the lastName and status of selected client

---
+++ nuxtapp/components/main-dash/client-tax-years/ClientTaxYearsHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="grid grid-cols-7 grid-rows-1 bg-gray-700 text-white rounded-t-sm px-3">
+      <div class=" col-start-1">
+        {{ selectedClient.lastName }}
+      </div>
+      <div class=" col-start-3">
+        {{ selectedClient.currentStatus }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import { models } from '~/shared/constants'
+
+export default {
+  name: 'ClientTaxYearsHeader',
+  computed: {
+    ...mapState([models.selectedClient])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
