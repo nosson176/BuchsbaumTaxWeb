43dda2c5d3f68a5844c5145a2edc4901e2f4266e
Yoshi Rubin <yoshrubin@gmail.com>
2021-10-07 16:35:15 +0300

    showing selectedClient

--- nuxtapp/components/main-dash/personal-contact/PersonalContact.vue
+++ nuxtapp/components/main-dash/personal-contact/PersonalContact.vue
@@ -3,8 +3,17 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import { models } from '~/shared/constants'
+
 export default {
-  name: 'PersonalContact'
+  name: 'PersonalContact',
+  computed: {
+    ...mapState([models.selectedClient])
+  },
+  created () {
+    console.log(this.selectedClient)
+  }
 }
 </script>
 
