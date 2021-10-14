24cee4f6d0958fe68a0949e1bcdaefda65a10498
Yoshi Rubin <yoshrubin@gmail.com>
2021-10-14 13:52:11 +0300

    redesigned tabs

--- nuxtapp/components/main-dash/tabs/Tab.vue
+++ nuxtapp/components/main-dash/tabs/Tab.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="w-40 p-1 cursor-pointer rounded-t bg-gray-400 text-white text-center" @click="emitClick">
+  <div
+    class="cursor-pointer border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+    :class="classObj"
+    @click="emitClick"
+  >
     <slot />
   </div>
 </template>
@@ -8,6 +12,18 @@
 import { events } from '~/shared/constants'
 export default {
   name: 'Tab',
+  props: {
+    active: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    classObj () {
+      const active = this.active
+      return { active }
+    }
+  },
   methods: {
     emitClick () {
       this.$emit(events.click)
@@ -18,4 +34,7 @@ export default {
 
 <style scoped>
 
+.active {
+  @apply border-indigo-500 text-gray-900;
+}
 </style>
