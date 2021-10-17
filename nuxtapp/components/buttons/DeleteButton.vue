d5f841fb3a64cda727c4397420f39aabafa8db17
Yoshi Rubin <yoshrubin@gmail.com>
2021-10-17 15:16:35 +0300

    made delete button component

---
+++ nuxtapp/components/buttons/DeleteButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex items-center justify-center">
+    <div class="rounded-full cursor-pointer bg-gray-200 w-5 h-5 leading-tight text-center">
+      <CloseIcon class="w-2 h-2" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
