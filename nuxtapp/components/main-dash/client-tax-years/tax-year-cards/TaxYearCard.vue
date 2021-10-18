beb17d5bff0223a030e77bb3061b77c820a2906e
Yoshi Rubin <yoshrubin@gmail.com>
2021-10-18 16:07:37 +0300

    starting on the taxYears

---
+++ nuxtapp/components/main-dash/client-tax-years/tax-year-cards/TaxYearCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'TaxYearCard'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ nuxtapp/components/main-dash/client-tax-years/tax-year-cards/TaxYearCardMenu.vue
@@ -0,0 +1,13 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'TaxYearCardMenu'
+}
+</script>
+
+<style scoped>
+
+</style>
