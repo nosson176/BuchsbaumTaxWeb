8398e9d1009d3a1fb00fe9ac34ee593500579089
Yoshi Rubin <yoshrubin@gmail.com>
2021-11-14 10:23:32 +0200

    adding loading indicator

---
+++ nuxtapp/components/LoadingIndicator.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="flex items-center justify-center px-3.5">
+      <div class="w-5 h-5 border-l-2 border-gray-50 rounded-full animate-spin" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoadingIndicator',
+
+  data () {
+    return {
+
+    }
+  },
+
+  mounted () {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- nuxtapp/components/modals/DeleteCard.vue
+++ nuxtapp/components/modals/DeleteCard.vue
@@ -11,7 +11,7 @@
           </h3>
           <div class="mt-2">
             <p class="text-sm text-gray-500">
-              Are you sure you want to delete this item?
+              Are you sure you want to {{ updateToValue }} this item?
             </p>
           </div>
         </div>
@@ -19,7 +19,8 @@
     </div>
     <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
       <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm" @click="handleDelete">
-        Delete
+        <LoadingIndicator v-if="isLoading" />
+        <span v-else>{{ updateToValue }}</span>
       </button>
       <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" @click="emitHide">
         Cancel
@@ -44,10 +45,21 @@ export default {
       required: true
     }
   },
+  data () {
+    return {
+      isLoading: false
+    }
+  },
   computed: {
     ...mapState([models.selectedClient]),
     clientId () {
       return this.selectedClient.id
+    },
+    headers () {
+      return this.$api.getHttpConfig()
+    },
+    updateToValue () {
+      return this.updatedItem.archived ? 'archive' : 'unarchive'
     },
     isTypeLog () {
       return this.type === tabs.logs
@@ -69,58 +81,63 @@ export default {
     },
     updatedLog () {
       const log = this.logs.find(log => log.id === this.id)
-      log.archived = true
+      log.archived = !log.archived
       return log
     },
     updatedIncome () {
       const income = this.incomes.find(income => income.id === this.id)
-      income.archived = true
+      income.archived = !income.archived
       return income
     },
     updatedFbar () {
       const fbar = this.fbars.find(fbar => fbar.id === this.id)
-      fbar.archived = true
+      fbar.archived = !fbar.archived
       return fbar
+    },
+    updatedItem () {
+      let item = null
+      if (this.isTypeLog) {
+        item = this.updatedLog
+      } else if (this.isTypeIncome) {
+        item = this.updatedIncome
+      } else if (this.isTypeFbar) {
+        item = this.updatedFbar
+      }
+      return item
     }
   },
   methods: {
     handleDelete () {
-      const headers = this.$api.getHttpConfig()
+      this.isLoading = true
       if (this.isTypeLog) {
-        this.updateLog(headers)
+        this.updateLog()
       } else if (this.isTypeIncome) {
-        this.updateIncome(headers)
+        this.updateIncome()
       } else if (this.isTypeFbar) {
-        this.updateFbar(headers)
+        this.updateFbar()
       }
     },
     emitHide () {
       this.$emit(events.hide)
     },
-    updateLog (headers) {
-      const log = this.updatedLog
-      const clientId = this.clientId
-      const logId = this.id
-      this.$api.updateLog(headers, { clientId, logId }, log)
-      this.reloadClient(headers, this.clientId)
+    updateLog () {
+      this.$api.updateLog(this.headers, { clientId: this.clientId, logId: this.id }, this.updatedItem)
+      this.reloadClient()
     },
-    updateIncome (headers) {
-      const income = this.updatedIncome
-      const clientId = this.clientId
-      const incomeId = this.id
-      this.$api.updateIncome(headers, { clientId, incomeId }, income)
-      this.reloadClient(headers, this.clientId)
+    updateIncome () {
+      this.$api.updateIncome(this.headers, { clientId: this.clientId, incomeId: this.id }, this.updatedItem)
+      this.reloadClient()
     },
-    updateFbar (headers) {
-      const fbar = this.updatedFbar
-      const clientId = this.clientId
-      const fbarId = this.id
-      this.$api.updateFbar(headers, { clientId, fbarId }, fbar)
-      this.reloadClient(headers, this.clientId)
+    updateFbar () {
+      this.$api.updateFbar(this.headers, { clientId: this.clientId, fbarId: this.id }, this.updatedItem)
+      this.reloadClient()
     },
-    reloadClient (headers, clientId) {
-      this.$api.getClientData(headers, clientId)
-      this.emitHide()
+    reloadClient () {
+      this.$api.getClientData(this.headers, this.clientId)
+        .then(() => {
+          this.isLoading = false
+          this.emitHide()
+        })
     }
   }
 }
