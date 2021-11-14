f284a0c5225d8e14c34f0813009adca139363981
Yoshi Rubin <yoshrubin@gmail.com>
2021-11-14 10:36:12 +0200

    nicer loading indicator

--- nuxtapp/components/LoadingIndicator.vue
+++ nuxtapp/components/LoadingIndicator.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
-    <div class="flex items-center justify-center px-3.5">
-      <div class="w-5 h-5 border-l-2 border-gray-50 rounded-full animate-spin" />
-    </div>
+    <svg class="animate-spin -ml-1 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <circle
+        class="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        stroke-width="4"
+      />
+      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
+    </svg>
   </div>
 </template>
 
