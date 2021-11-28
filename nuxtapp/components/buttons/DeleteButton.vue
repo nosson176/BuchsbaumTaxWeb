39dfb0b78fa98466353fe95992d86b9ac38fcd32
Yoshi Rubin <yoshrubin@gmail.com>
2021-11-28 09:29:52 +0200

    made delete button work with tab and enter

--- nuxtapp/components/buttons/DeleteButton.vue
+++ nuxtapp/components/buttons/DeleteButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center justify-center">
-    <div class="rounded-full cursor-pointer bg-gray-500 text-white w-5 h-5 leading-tight text-center" @click="emitClick">
+    <div class="rounded-full cursor-pointer bg-gray-500 text-white w-5 h-5 leading-tight text-center" tabindex="0" @click="emitClick" @keypress.enter="emitClick">
       <CloseIcon class="w-5 h-5" />
     </div>
   </div>
