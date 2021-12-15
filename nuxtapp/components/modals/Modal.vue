223742bc6f1a417c3b2e1898b3d11765f036509c
Yoshi Rubin <yoshrubin@gmail.com>
2021-12-15 17:28:14 +0200

    not sure why this is not on top of everything

--- nuxtapp/components/modals/Modal.vue
+++ nuxtapp/components/modals/Modal.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- This example requires Tailwind CSS v2.0+ -->
-    <div v-if="showing" class="fixed z-50 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+    <div v-if="showing" class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
       <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <!--
       Entering: "ease-out duration-300"
