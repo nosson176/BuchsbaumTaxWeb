73581f6d816d9db4220518124aba15674e93d803
Yoshi Rubin <yoshrubin@gmail.com>
2021-12-15 17:29:18 +0200

    will put the input into a card like I did for delete

---
+++ nuxtapp/components/modals/SingleInputCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'SingleInputCard',
+
+  data () {
+    return {
+
+    }
+  },
+
+  mounted () {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
