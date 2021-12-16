a007dbea8d0033e971bcc6053f138995a8dde4db
Yoshi Rubin <yoshrubin@gmail.com>
2021-12-16 09:00:34 +0200

    appears the remember me checkbox was not working

--- nuxtapp/components/login/Login.vue
+++ nuxtapp/components/login/Login.vue
@@ -35,7 +35,7 @@
 
       <div class="flex items-center justify-between">
         <div class="flex items-center">
-          <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-yellow-600 focus:ring-yellow-500 border-gray-300 rounded">
+          <input id="remember-me" v-model="formModel.rememberMe" name="remember-me" type="checkbox" class="h-4 w-4 text-yellow-600 focus:ring-yellow-500 border-gray-300 rounded">
           <label for="remember-me" class="ml-2 block text-sm text-gray-900">
             Remember me
           </label>
