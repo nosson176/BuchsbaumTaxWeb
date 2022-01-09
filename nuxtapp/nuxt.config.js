8241a926a3e3a071a6cbad72ea12a279b86ec849
Yoshi Rubin <yoshrubin@gmail.com>
2022-01-09 13:51:04 +0200

    added date picker to post css

--- nuxtapp/nuxt.config.js
+++ nuxtapp/nuxt.config.js
@@ -30,7 +30,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/css/global.css'
+    '~/assets/css/global.css',
+    '~/assets/css/date-picker.css'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
