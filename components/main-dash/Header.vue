119c06438f19763085a37733bb437dc8721ece61
Yoshi Rubin <yoshrubin@gmail.com>
2022-02-27 17:50:19 +0200

    adding navigation stylig

--- components/main-dash/Header.vue
+++ components/main-dash/Header.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
-    <div class="bg-gray-800 text-white w-full flex justify-evenly items-center h-10 z-10 shadow">
-      <Dropdown shown-value="History" :options="mappedClientHistory" @input="getSelectedClient" />
+    <div class="bg-gray-800 text-white w-full flex justify-center items-center h-10 z-10 shadow px-4">
+      <div class="ml-auto">
+        <Dropdown shown-value="History" :options="mappedClientHistory" @input="getSelectedClient" />
+      </div>
+      <div class="ml-auto">
+        <nuxt-link :to="homeRoute">
+          <HomeIcon class="w-8 h-8 cursor-pointer" />
+        </nuxt-link>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
-import { models } from '~/shared/constants'
+import { models, routes } from '~/shared/constants'
 
 export default {
   name: 'Header',
@@ -29,6 +36,11 @@ export default {
     },
     clientsHistoryLoaded () {
       return !Array.isArray(this.clientsHistory.length) || !this.clientsHistory.length.length
+    },
+    homeRoute () {
+      return {
+        name: routes.home
+      }
     }
   },
   methods: {
@@ -44,5 +56,7 @@ export default {
 </script>
 
 <style scoped>
-
+a.nuxt-link-active {
+  @apply w-8 h-8 text-indigo-500;
+}
 </style>
