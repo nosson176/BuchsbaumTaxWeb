6128ede28dc2545ccbc2b155a47319396fa76e24
Yoshi Rubin <yoshrubin@gmail.com>
2022-03-02 16:52:04 +0200

    added create endpoint

--- plugins/api.js
+++ plugins/api.js
@@ -1,5 +1,11 @@
 import { getCookieByKey, setCookieByKey } from '~/shared/cookie-utilities'
-import { COOKIE_KEY_SESSION_TOKEN, error, models, mutations, routes } from '~/shared/constants'
+import {
+  COOKIE_KEY_SESSION_TOKEN,
+  error,
+  models,
+  mutations,
+  routes,
+} from '~/shared/constants'
 
 export default ({ $axios, store, $toast, redirect }, inject) => {
   const getHeaders = () => {
@@ -11,13 +17,18 @@ export default ({ $axios, store, $toast, redirect }, inject) => {
     return headers
   }
 
-  const login = data => $axios.post('/sessions', data, { loading: models.token, store: models.token })
-    .catch(() => $toast.error('Invalid email or password'))
+  const login = (data) =>
+    $axios
+      .post('/sessions', data, { loading: models.token, store: models.token })
+      .catch(() => $toast.error('Invalid email or password'))
 
   const signout = () => {
     const token = ''
     setCookieByKey(COOKIE_KEY_SESSION_TOKEN, token, { expires: 365 })
-    store.commit(mutations.setModelResponse, { model: models.token, data: { token } })
+    store.commit(mutations.setModelResponse, {
+      model: models.token,
+      data: { token },
+    })
   }
 
   // GET
@@ -30,7 +41,12 @@ export default ({ $axios, store, $toast, redirect }, inject) => {
     } else if (searchOption) {
       endpoint = endpoint.concat(`?field=${searchOption}`)
     }
-    return $axios.get(endpoint, { headers, loading: models.clients, store: models.clients })
+    return $axios
+      .get(endpoint, {
+        headers,
+        loading: models.clients,
+        store: models.clients,
+      })
       .catch((e) => {
         $toast.error('Error loading clients')
         if (e.message === error.axios_401) {
@@ -40,120 +56,194 @@ export default ({ $axios, store, $toast, redirect }, inject) => {
       })
   }
 
-  const getClientData = (headers, id) => $axios.get(`/clients/${id}/data`, { headers, loading: models.selectedClient, store: models.selectedClient })
-    .then(() => getClientsHistory(headers))
-    .catch(() => $toast.error('Error loading client data'))
+  const getClientData = (headers, id) =>
+    $axios
+      .get(`/clients/${id}/data`, {
+        headers,
+        loading: models.selectedClient,
+        store: models.selectedClient,
+      })
+      .then(() => getClientsHistory(headers))
+      .catch(() => $toast.error('Error loading client data'))
 
-  const getValueTypes = headers => $axios.get('values', { headers, loading: models.valueTypes, store: models.valueTypes })
-    .catch(() => $toast.error('Error loading value types'))
+  const getValueTypes = (headers) =>
+    $axios
+      .get('values', {
+        headers,
+        loading: models.valueTypes,
+        store: models.valueTypes,
+      })
+      .catch(() => $toast.error('Error loading value types'))
 
-  const getValueTaxGroups = headers => $axios.get('values/tax-groups', { headers, loading: models.valueTaxGroups, store: models.valueTaxGroups })
-    .catch(() => $toast.error('Error loading value tax groups'))
+  const getValueTaxGroups = (headers) =>
+    $axios
+      .get('values/tax-groups', {
+        headers,
+        loading: models.valueTaxGroups,
+        store: models.valueTaxGroups,
+      })
+      .catch(() => $toast.error('Error loading value tax groups'))
 
-  const getAllClientFees = headers => $axios.get('/fees', { headers, loading: models.allClientFees, store: models.allClientFees })
-    .catch(() => $toast.error('Error loading all client fees'))
+  const getAllClientFees = (headers) =>
+    $axios
+      .get('/fees', {
+        headers,
+        loading: models.allClientFees,
+        store: models.allClientFees,
+      })
+      .catch(() => $toast.error('Error loading all client fees'))
 
-  const getAllUsers = headers => $axios.get('/users', { headers, loading: models.users, store: models.users })
-    .catch(() => $toast.error('Error loading all users'))
+  const getAllUsers = (headers) =>
+    $axios
+      .get('/users', { headers, loading: models.users, store: models.users })
+      .catch(() => $toast.error('Error loading all users'))
 
-  const getClientsHistory = headers => $axios.get('/clients/history', { headers, loading: models.clientsHistory, store: models.clientsHistory })
-    .catch(() => $toast.error('Error loading clients history'))
+  const getClientsHistory = (headers) =>
+    $axios
+      .get('/clients/history', {
+        headers,
+        loading: models.clientsHistory,
+        store: models.clientsHistory,
+      })
+      .catch(() => $toast.error('Error loading clients history'))
 
-  const getSmartviews = headers => $axios.get('/smartviews', { headers, loading: models.smartviews, store: models.smartviews })
-    .catch(() => $toast.error('Error loading smartviews'))
+  const getSmartviews = (headers) =>
+    $axios
+      .get('/smartviews', {
+        headers,
+        loading: models.smartviews,
+        store: models.smartviews,
+      })
+      .catch(() => $toast.error('Error loading smartviews'))
 
   // CREATE
-  const createLog = (headers, { log }) => $axios.post('/logs', log, { headers })
-    .catch(() => $toast.error('Error creating log'))
+  const createLog = (headers, { log }) =>
+    $axios
+      .post('/logs', log, { headers })
+      .catch(() => $toast.error('Error creating log'))
 
-  const createTaxPersonal = (headers, { personal }) => $axios.post('/personals', personal, { headers })
-    .catch(() => $toast.error('Error creating tax personal'))
+  const createTaxPersonal = (headers, { personal }) =>
+    $axios
+      .post('/personals', personal, { headers })
+      .catch(() => $toast.error('Error creating tax personal'))
 
-  const createContact = (headers, { contact }) => $axios.post('/contacts', contact, { headers })
-    .catch(() => $toast.error('Error creating contact'))
+  const createContact = (headers, { contact }) =>
+    $axios
+      .post('/contacts', contact, { headers })
+      .catch(() => $toast.error('Error creating contact'))
 
-  const createIncome = (headers, { income }) => $axios.post('/incomes', income, { headers })
-    .catch(() => $toast.error('Error creating income'))
+  const createIncome = (headers, { income }) =>
+    $axios
+      .post('/incomes', income, { headers })
+      .catch(() => $toast.error('Error creating income'))
 
-  const createFbar = (headers, { fbar }) => $axios.post('/fbars', fbar, { headers })
-    .catch(() => $toast.error('Error creating fbar'))
+  const createFbar = (headers, { fbar }) =>
+    $axios
+      .post('/fbars', fbar, { headers })
+      .catch(() => $toast.error('Error creating fbar'))
 
-  const createFee = (headers, { fee }) => $axios.post('/fees', fee, { headers })
-    .catch(() => $toast.error('Error creating fee'))
+  const createFee = (headers, { fee }) =>
+    $axios
+      .post('/fees', fee, { headers })
+      .catch(() => $toast.error('Error creating fee'))
 
-  const createTaxYear = (headers, { taxYear }) => $axios.post('/tax-years', taxYear, { headers })
-    .catch(() => $toast.error('Error creating tax year'))
+  const createTaxYear = (headers, { taxYear }) =>
+    $axios
+      .post('/tax-years', taxYear, { headers })
+      .catch(() => $toast.error('Error creating tax year'))
 
-  const createFiling = (headers, { filing }) => $axios.post('/filings', filing, { headers })
-    .catch(() => $toast.error('Error creating filing'))
+  const createFiling = (headers, { filing }) =>
+    $axios
+      .post('/filings', filing, { headers })
+      .catch(() => $toast.error('Error creating filing'))
 
-  const createChecklist = (headers, { checklist }) => $axios.post('/checklists', checklist, { headers })
-    .catch(() => $toast.error('Error creating checklist'))
+  const createChecklist = (headers, { checklist }) =>
+    $axios
+      .post('/checklists', checklist, { headers })
+      .catch(() => $toast.error('Error creating checklist'))
 
-  const createClient = (headers, { client }) => $axios.post('/clients', client, { headers })
-    .catch(() => $toast.error('Error creating client'))
+  const createClient = (headers, { client }) =>
+    $axios
+      .post('/clients', client, { headers })
+      .catch(() => $toast.error('Error creating client'))
 
-  const createSmartview = (headers, { smartview }) => $axios.post('/smartviews', smartview, { headers })
-    .catch(() => $toast.error('Error creating smartview'))
-    .finally(() => getSmartviews(headers))
+  const createSmartview = (headers, { smartview }) =>
+    $axios
+      .post('/smartviews', smartview, { headers })
+      .catch(() => $toast.error('Error creating smartview'))
+      .finally(() => getSmartviews(headers))
+
+  const createValue = (headers, { value }) =>
+    $axios.post('/values', value, { headers })
 
   // UPDATE
   const updateClient = (headers, { clientId, client }) =>
-    $axios.put(`/clients/${clientId}`, client, { headers })
+    $axios
+      .put(`/clients/${clientId}`, client, { headers })
       .catch(() => $toast.error('Error updating client'))
       .finally(() => getClientData(headers, clientId))
 
   const updateLog = (headers, { clientId, logId }, log) =>
-    $axios.put(`/logs/${logId}`, log, { headers })
+    $axios
+      .put(`/logs/${logId}`, log, { headers })
       .catch(() => $toast.error('Error updating log'))
       .finally(() => getClientData(headers, clientId))
 
   const updateTaxPersonal = (headers, { clientId, personalId }, personal) =>
-    $axios.put(`/personals/${personalId}`, personal, { headers })
+    $axios
+      .put(`/personals/${personalId}`, personal, { headers })
       .catch(() => $toast.error('Error updating tax personal'))
       .finally(() => getClientData(headers, clientId))
 
   const updateContact = (headers, { clientId, contactId = '' }, contact) =>
-    $axios.put(`/contacts/${contactId}`, contact, { headers })
+    $axios
+      .put(`/contacts/${contactId}`, contact, { headers })
       .catch(() => $toast.error('Error updating contact'))
       .finally(() => getClientData(headers, clientId))
 
   const updateIncome = (headers, { clientId, incomeId = '' }, income) => {
     const endpoint = incomeId ? `/incomes/${incomeId}` : '/incomes'
-    $axios.put(endpoint, income, { headers })
+    $axios
+      .put(endpoint, income, { headers })
       .catch(() => $toast.error('Error updating income'))
       .finally(() => getClientData(headers, clientId))
   }
 
   const updateFbar = (headers, { clientId, fbarId = '' }, fbar) => {
     const endpoint = fbarId ? `/fbars/${fbarId}` : '/fbars'
-    return $axios.put(endpoint, fbar, { headers })
+    return $axios
+      .put(endpoint, fbar, { headers })
       .catch(() => $toast.error('Error updating fbar'))
       .finally(() => getClientData(headers, clientId))
   }
 
   const updateFee = (headers, { clientId, feeId }, fee) =>
-    $axios.put(`/fees/${feeId}`, fee, { headers })
+    $axios
+      .put(`/fees/${feeId}`, fee, { headers })
       .catch(() => $toast.error('Error updating fee'))
       .finally(() => getClientData(headers, clientId))
 
   const updateFiling = (headers, { clientId, filingId }, filing) =>
-    $axios.put(`/filings/${filingId}`, filing, { headers })
+    $axios
+      .put(`/filings/${filingId}`, filing, { headers })
       .catch(() => $toast.error('Error updating filing'))
       .finally(() => getClientData(headers, clientId))
 
   const updateTaxYear = (headers, { clientId, taxYearId }, taxYear) =>
-    $axios.put(`/tax-years/${taxYearId}`, taxYear, { headers })
+    $axios
+      .put(`/tax-years/${taxYearId}`, taxYear, { headers })
       .catch(() => $toast.error('Error updating tax year'))
       .finally(() => getClientData(headers, clientId))
 
   const updateChecklist = (headers, { clientId, checklistId }, checklist) =>
-    $axios.put(`/checklists/${checklistId}`, checklist, { headers })
+    $axios
+      .put(`/checklists/${checklistId}`, checklist, { headers })
       .catch(() => $toast.error('Error updating checklist'))
       .finally(() => getClientData(headers, clientId))
 
   const updateSmartview = (headers, { smartviewId }, smartview) =>
-    $axios.put(`/smartviews/${smartviewId}`, smartview, { headers })
+    $axios
+      .put(`/smartviews/${smartviewId}`, smartview, { headers })
       .catch(() => $toast.error('Error updating smartview'))
       .finally(() => getSmartviews(headers))
 
@@ -166,6 +256,7 @@ export default ({ $axios, store, $toast, redirect }, inject) => {
     createFiling,
     createIncome,
     createLog,
+    createValue,
     createSmartview,
     createTaxPersonal,
     createTaxYear,
@@ -190,7 +281,7 @@ export default ({ $axios, store, $toast, redirect }, inject) => {
     updateLog,
     updateSmartview,
     updateTaxPersonal,
-    updateTaxYear
+    updateTaxYear,
   }
 
   // Inject to context as $api
