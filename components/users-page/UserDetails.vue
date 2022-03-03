3ac6ac82fe37cc8b86021099f3eb57260c9a115a
Yoshi Rubin <yoshrubin@gmail.com>
2022-03-03 17:00:05 +0200

    will finish the work on the displaying of the data

--- components/users-page/UserDetails.vue
+++ components/users-page/UserDetails.vue
@@ -1,16 +1,51 @@
 <template>
-  <div>{{ user }}</div>
+  <div>
+    <section aria-labelledby="user-information-title">
+      <div class="bg-white">
+        <div class="px-4 py-5 bg-gray-50 sm:px-6">
+          <h2
+            id="user-information-title"
+            class="text-lg leading-6 font-medium text-gray-900"
+            contenteditable
+          >{{ user.username }}</h2>
+        </div>
+        <div class="border-t border-gray-200 px-4 py-5 sm:px-6">
+          <dl class="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+            <div class="sm:col-span-1">
+              <dt class="text-sm font-medium text-gray-500">Privelage Set</dt>
+              <dd contenteditable class="mt-1 text-sm text-gray-900">{{ user.userType }}</dd>
+            </div>
+            <div class="sm:col-span-1">
+              <dt class="text-sm font-medium text-gray-500">Allow Texting</dt>
+              <dd class="mt-1 text-sm text-gray-900">
+                <CheckBoxToDisplayTrueFalse :value="allowTexting" />
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'UserDetails',
+  name: "UserDetails",
   props: {
     user: {
       type: Object,
       default: () => null,
     }
   },
+  computed: {
+    allowTexting: {
+      get () {
+        return this.user.allowTexting;
+      }, set (value) {
+        this.formModel.allowTexting = value;
+      }
+    }
+  }
 };
 </script>
 
