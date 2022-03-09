c46b31bad74c56f068e91d8ede120e4a1f02dce3
Yoshi Rubin <yoshrubin@gmail.com>
2022-03-09 09:04:26 +0200

    adding client tables and use fetch

---
+++ components/values-page/tables/client/ClientTables.vue
@@ -0,0 +1,14 @@
+<template>
+  <ValuesTableGrid>
+    <ClientStatusTable />
+  </ValuesTableGrid>
+</template>
+
+<script>
+export default {
+  name: 'ClientValueTables',
+};
+</script>
+
+<style scoped>
+</style>
