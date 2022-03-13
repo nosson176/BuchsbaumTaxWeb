c72adea1ec1d02fccc2bf75c255382368c783a60
Yoshi Rubin <yoshrubin@gmail.com>
2022-03-13 15:10:37 +0200

    worked out the spacing a little better in the header options select

--- components/form-components/HeaderSelectOption.vue
+++ components/form-components/HeaderSelectOption.vue
@@ -15,30 +15,51 @@
         >
           <span class="sr-only">Open options</span>
           <!-- Heroicon name: solid/dots-vertical -->
-          <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-            <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
+          <svg
+            class="h-4 w-4"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"
+            />
           </svg>
         </button>
       </div>
       <div v-else>
         <input
-
           ref="filter"
           v-model="inputValue"
           type="text"
-          class="bg-white text-xs text-gray-900 w-full border border-gray-300 rounded-md shadow-sm pl-1 pr-4 py-0.5 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+          class="bg-white text-xs text-gray-900 w-full border border-gray-300 rounded-md shadow-sm pl-0.5 pr-0.5 py-0.5 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
           @keyup="onInputKeyup($event.key)"
           @click="onInputClick"
-        >
-        <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+        />
+        <span class="absolute inset-y-0 right-0 flex items-center pr-0 pointer-events-none">
           <!-- Heroicon name: solid/selector -->
-          <svg class="h-3 w-3 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-            <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+          <svg
+            class="h-3 w-3 text-gray-400"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            />
           </svg>
         </span>
       </div>
     </div>
-    <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
+    <transition
+      leave-active-class="transition ease-in duration-100"
+      leave-from-class="opacity-100"
+      leave-to-class="opacity-0"
+    >
       <ul
         v-if="showOptions"
         class="absolute z-20 mt-1 w-auto bg-white text-xs shadow-lg rounded-md py-1 ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none"
@@ -59,13 +80,26 @@
           role="option"
           @click="setSelectOption(option)"
         >
-          <span :class="[isSelected(option) ? 'font-semibold' : 'font-normal','block truncate']">
-            {{ option.name || option.value }}
-          </span>
-          <span v-if="isSelected(option)" class="text-indigo-600 absolute inset-y-0 right-0 flex items-center pr-4 hover:text-white">
+          <span
+            :class="[isSelected(option) ? 'font-semibold' : 'font-normal', 'block truncate']"
+          >{{ option.name || option.value }}</span>
+          <span
+            v-if="isSelected(option)"
+            class="text-indigo-600 absolute inset-y-0 right-0 flex items-center pr-4 hover:text-white"
+          >
             <!-- Heroicon name: solid/check -->
-            <svg class="h-3 w-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+            <svg
+              class="h-3 w-3"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              />
             </svg>
           </span>
         </li>
@@ -75,7 +109,8 @@
 </template>
 
 <script>
-import { events } from '~/shared/constants'
+import { mapState } from 'vuex'
+import { events, models } from '~/shared/constants'
 export default {
   name: 'HeaderSelectOption',
   props: {
@@ -105,6 +140,7 @@ export default {
     }
   },
   computed: {
+    ...mapState([models.selectedClient]),
     computedValue: {
       get () {
         return this.value
@@ -115,7 +151,7 @@ export default {
     },
     inputValue: {
       get () {
-        return this.showOptions ? this.filterOptionsValue : this.shownValue
+        return this.filterOptionsValue
       },
       set (value) {
         this.filterOptionsValue = value
@@ -137,6 +173,11 @@ export default {
       if (value) {
         this.$refs.filter?.focus()
         this.inputValue = ''
+      }
+    },
+    selectedClient (newVal, oldVal) {
+      if (newVal.id !== oldVal.id) {
+        this.filterOptionsValue = ''
       }
     }
   },
