6e53997efc8707bbb8b0f35876424590bd86c4cf
Yoshi Rubin <yoshrubin@gmail.com>
2022-03-16 17:01:37 +0200

    better spacing on fbar table

--- components/main-dash/logs-income-fbar/tables/FbarTable.vue
+++ components/main-dash/logs-income-fbar/tables/FbarTable.vue
@@ -2,7 +2,7 @@
   <Table v-if="isClientSelected" @keydown.tab.prevent="onKeyDown">
     <template #header>
       <TableHeader>
-        <div class="table-header xs flex flex-col">
+        <div class="table-header w-6 flex flex-col">
           <AddRowButton @click="onAddRowClick" />
           <HeaderSelectOption
             v-model="includeAll"
@@ -11,7 +11,7 @@
             @input="handleUpdateIncludeAll"
           />
         </div>
-        <div class="table-header xs" />
+        <div class="table-header w-6" />
         <div class="table-header xs flex flex-col">
           <div class="flex items-center space-x-0.5">
             <span>Year</span>
@@ -40,7 +40,7 @@
           </div>
           <HeaderSelectOption v-model="typeFilterValue" :options="filteredTypeOptions" />
         </div>
-        <div class="table-header xs flex flex-col">
+        <div class="table-header sm flex flex-col">
           <div class="flex items-center space-x-0.5">
             <span>Job</span>
             <DeleteButton small @click="jobFilterValue = ''" />
@@ -58,7 +58,7 @@
         <div class="table-header xs">X</div>
         <div class="table-header sm">$</div>
         <div class="table-header xs">Doc</div>
-        <div class="table-header lg flex flex-col">
+        <div class="table-header xl flex flex-col">
           <div class="flex items-center space-x-0.5">
             <span>Description</span>
             <DeleteButton small @click="descriptionFilterValue = ''" />
@@ -79,12 +79,12 @@
         :idx="idx"
         :class="{ 'disabled': !fbar.include, 'selected': isSelected(fbar.id) }"
       >
-        <div class="table-col bg-gray-200 mr-1">
+        <div class="table-col w-6 bg-gray-200">
           <ClickCell @click="toggleSelected(fbar)">{{ idx + 1 }}</ClickCell>
         </div>
         <div
           :id="`${idx}-include`"
-          class="table-col xs"
+          class="table-col w-6 p-1"
           @click="toggleEditable(`${idx}-include`, fbar.id)"
         >
           <EditableCheckBoxCell
@@ -145,7 +145,7 @@
             @input="debounceUpdate"
           />
         </div>
-        <div :id="`${idx}-job`" class="table-col xs" @click="toggleEditable(`${idx}-job`, fbar.id)">
+        <div :id="`${idx}-job`" class="table-col sm" @click="toggleEditable(`${idx}-job`, fbar.id)">
           <EditableSelectCell
             v-model="fbar.job"
             :is-editable="isEditable(`${idx}-job`)"
@@ -218,7 +218,7 @@
         </div>
         <div
           :id="`${idx}-description`"
-          class="table-col lg"
+          class="table-col xl"
           @click="toggleEditable(`${idx}-description`, fbar.id)"
         >
           <EditableInputCell
@@ -246,18 +246,18 @@
       </TableRow>
       <TableRow class="sticky bottom-0 bg-gray-300 shadow">
         <div class="table-col w-6" />
-        <div class="table-col xs" />
+        <div class="table-col w-6" />
         <div class="table-col-primary xs" />
         <div class="table-col xs" />
         <div class="table-col sm" />
         <div class="table-col sm" />
-        <div class="table-col xs" />
+        <div class="table-col sm" />
         <div class="sm table-col-primary">{{ amountTotal }}</div>
         <div class="table-col xs" />
         <div class="table-col xs" />
         <div class="table-col-primary sm">{{ amountUSDTotal }}</div>
         <div class="table-col xs" />
-        <div class="table-col lg" />
+        <div class="table-col xl" />
         <div class="table-col sm" />
         <div class="table-col xs" />
       </TableRow>
