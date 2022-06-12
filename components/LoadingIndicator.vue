d6a9413d3ed97c45b0b91b03c9f6a0c219309922
Yoshi Rubin <yoshrubin@gmail.com>
2022-06-12 17:31:31 +0300

    made loading indicator more flexible to style

--- components/LoadingIndicator.vue
+++ components/LoadingIndicator.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-    <svg
-      class="animate-spin -ml-1 h-5 w-5 text-white"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-    >
+    <svg class="animate-spin -ml-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
       <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
       <path
         class="opacity-75"
