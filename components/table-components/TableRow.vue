230d50dc853927296a7cf94f6d23255c61aa812c
Yoshi Rubin <yoshrubin@gmail.com>
2022-07-25 14:15:17 +0300

    emit drag events

--- components/table-components/TableRow.vue
+++ components/table-components/TableRow.vue
@@ -1,16 +1,32 @@
 <template>
-  <div class="flex items-center bg-gray-50" :class="idx % 2 === 0 ? 'even' : ''">
+  <div
+    :draggable="draggable"
+    class="flex items-center bg-gray-50"
+    :class="idx % 2 === 0 ? 'even' : ''"
+    @dragstart="emitDragstart($event)"
+  >
     <slot />
   </div>
 </template>
 
 <script>
+import { events } from '@/shared/constants'
+
 export default {
   name: 'TableRow',
   props: {
     idx: {
       type: Number,
       default: 0,
+    },
+    draggable: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    emitDragstart(evt) {
+      this.$emit(events.dragstart, evt)
     },
   },
 }
