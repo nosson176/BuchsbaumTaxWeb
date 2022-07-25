9b017db0b2521d9bca12ee03d40bcef2c55a2acb
Yoshi Rubin <yoshrubin@gmail.com>
2022-07-25 16:51:37 +0300

    add click event to table row

--- components/table-components/TableRow.vue
+++ components/table-components/TableRow.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="flex items-center bg-gray-50" :class="idx % 2 === 0 ? 'even' : ''">
+  <div class="flex items-center bg-gray-50" :class="idx % 2 === 0 ? 'even' : ''" @click="emitClick">
     <slot />
   </div>
 </template>
 
 <script>
+import { events } from '~/shared/constants'
 export default {
   name: 'TableRow',
   props: {
     idx: {
       type: Number,
       default: 0,
+    },
+  },
+  methods: {
+    emitClick(e) {
+      this.$emit(events.click, e)
     },
   },
 }
