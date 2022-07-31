37e8fd70e8b4436201c81d3469357be93f45aac9
Yoshi Rubin <yoshrubin@gmail.com>
2022-07-31 10:48:36 +0300

    added long option to the headerdropdown

--- components/modals/SmartviewLine .vue
+++ components/modals/SmartviewLine .vue
@@ -4,7 +4,7 @@
       <Input v-model="groupNum" />
     </div>
     <div class="col-span-5">
-      <HeaderSelectOption v-model="fieldName" :options="smartviewOptions" />
+      <HeaderSelectOption v-model="fieldName" long :options="smartviewOptions" />
     </div>
     <div class="col-span-2">
       <HeaderSelectOption v-model="operator" :options="operators" />
