91feb48ab9cd77ee5b28da7bb517ade8fa20cdb0
Yoshi Rubin <yoshrubin@gmail.com>
2022-07-31 12:26:04 +0300

    add drag and drop functionality

--- components/main-dash/client-tax-years/tax-year-cards/ClientTaxYearCardTabs.vue
+++ components/main-dash/client-tax-years/tax-year-cards/ClientTaxYearCardTabs.vue
@@ -1,27 +1,30 @@
 <template>
-  <div class="border-t border-gray-300 space-x-1 flex shadow">
-    <Tab
-      v-for="(filing, idx) in filings"
-      :key="idx"
-      :active="idx === activeFilingIdx"
-    >
-      <span
-        v-if="displayTab(filing)"
-        class="tab-text"
-        :class="{'text-gray-300': !filing.taxForm && !filing.state}"
-        @click="handleClick(idx)"
-      >
-        {{ filing.taxForm || filing.state || filing.filingType }}
-      </span>
-    </Tab>
+  <div class="border-t border-gray-300 space-x-1 shadow">
+    <draggable :value="filings" v-bind="dragOptions" @start="startDrag" @end="onDrop">
+      <transition-group type="transition" :name="transitionName">
+        <Tab v-for="(filing, idx) in filings" :key="filing.id" :active="idx === activeFilingIdx">
+          <span
+            v-if="displayTab(filing)"
+            class="tab-text"
+            :class="{ 'text-gray-300': !filing.taxForm && !filing.state }"
+            @click="handleClick(idx)"
+          >
+            {{ filing.taxForm || filing.state || filing.filingType }}
+          </span>
+        </Tab>
+      </transition-group>
+    </draggable>
   </div>
 </template>
 
 <script>
-import { events, filingTypes } from '~/shared/constants'
+import { mapState } from 'vuex'
+import draggable from 'vuedraggable'
+import { events, filingTypes, models } from '~/shared/constants'
 
 export default {
   name: 'ClientTaxYearCardTabs',
+  components: { draggable },
   props: {
     filings: {
       type: Array,
@@ -36,6 +39,22 @@ export default {
       default: () => null,
     },
   },
+  data() {
+    return {
+      dragActive: false,
+      dragOptions: {
+        animation: 200,
+        ghostClass: 'ghost',
+        direction: 'vertical',
+      },
+    }
+  },
+  computed: {
+    ...mapState([models.selectedClient]),
+    headers() {
+      return this.$api.getHeaders()
+    },
+  },
   methods: {
     handleClick(filingIdx) {
       this.emitClick(filingIdx)
@@ -45,17 +64,24 @@ export default {
     },
     displayTab(filing) {
       return filing.filingType !== filingTypes.ext && filing.filingType !== filingTypes.fbar
-    }
-  }
+    },
+    startDrag() {
+      this.dragActive = true
+    },
+    onDrop(evt) {
+      const item = JSON.parse(JSON.stringify(this.filings))[evt.oldIndex]
+      item.sortOrder = evt.newIndex + 1
+      this.$api.updateFiling(this.headers, { clientId: this.selectedClient.id, filingId: item.id }, item)
+      this.dragActive = false
+    },
+  },
 }
 </script>
 
 <style scoped>
-
 .tab-text {
   @apply uppercase;
 
   font-size: 10px;
 }
-
 </style>
