210c8efecc1ebc5da5b2bbd7e2cc458d827a9bbc
Yoshi Rubin <yoshrubin@gmail.com>
2022-08-15 11:13:30 +0300

    a little smoother updating and tabbing

--- components/main-dash/logs-income-fbar/tables/LogsTable.vue
+++ components/main-dash/logs-income-fbar/tables/LogsTable.vue
@@ -325,7 +325,7 @@ export default {
       const parsedDate = parseISO(date)
       return isToday(parsedDate) || isPast(parsedDate)
     },
-    async toggleComplete(log) {
+    toggleComplete(log) {
       if (!this.isTodayOrPast(log.alarmDate) && !log.alarmComplete) {
         return
       } else if (log.alarmComplete) {
@@ -334,7 +334,7 @@ export default {
         log.alarmComplete = true
       }
       this.editableLogId = log.id
-      await this.handleUpdate()
+      this.handleUpdate()
     },
     filterLogs(log) {
       const hasAlarm = log.alarmDate
@@ -353,8 +353,8 @@ export default {
     isSelected(logId) {
       return this.selectedItems[logId]
     },
-    async onTabPress() {
-      await this.handleUpdate()
+    onTabPress() {
+      this.handleUpdate()
       const currentCell = this.editableId
       const idArr = currentCell.split('-')
       const columnIndex = columns.findIndex((col) => col === idArr[1])
@@ -406,14 +406,13 @@ export default {
       this.$store.commit(mutations.setModelResponse, { model: models.secondsSpentOnClient, data: 0 })
       this.$store.commit(mutations.setModelResponse, { model: models.promptOnClientChange, data: true })
     },
-    async updateSecondsSpent(log) {
+    updateSecondsSpent(log) {
       const timeArr = log.timeSpent.split(':')
       if (timeArr.length < 2 || isNaN(timeArr[0]) || isNaN(timeArr[1])) {
         log.timeSpent = this.getTimeSpentOnClient(log)
         return false
       }
       log.secondsSpent = timeArr[0] * 3600 + timeArr[1] * 60
-      await this.handleUpdate()
       this.onBlur()
     },
   },
