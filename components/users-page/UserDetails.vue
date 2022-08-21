22f21af7f24cea0f8576011e9f796225b51c4a48
Yoshi Rubin <yoshrubin@gmail.com>
2022-08-21 12:49:08 +0300

    implementing editability

--- components/users-page/UserDetails.vue
+++ components/users-page/UserDetails.vue
@@ -11,35 +11,40 @@
           <dl class="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
             <div class="sm:col-span-1">
               <dt class="text-sm font-medium text-gray-500">First Name</dt>
-              <dd class="mt-1 text-sm text-gray-900" @click="toggleEditable('type')">
-                <EditableInput v-model="firstName" :is-editable="isFirstNameEditable" @blur="onBlur" />
+              <dd class="mt-1 text-sm text-gray-900">
+                <input v-model="firstName" @blur="onBlur" />
               </dd>
             </div>
             <div class="sm:col-span-1">
               <dt class="text-sm font-medium text-gray-500">Last Name</dt>
               <dd class="mt-1 text-sm text-gray-900">
-                <EditableInput v-model="lastName" :is-editable="isLastNameEditable" @blur="onBlur" />
+                <input v-model="lastName" @blur="onBlur" />
               </dd>
             </div>
           </dl>
           <dl class="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
-            <div class="sm:col-span-1">
-              <dt class="text-sm font-medium text-gray-500">Privelage Set</dt>
-              <dd class="mt-1 text-sm text-gray-900" @click="toggleEditable('type')">
-                <EditableSelectCell v-model="userType" :is-editable="isTypeEditable" :options="[]" @blur="onBlur" />
-              </dd>
+            <div class="sm:col-span-1" @click="toggleEditable('type')">
+              <label class="text-sm font-medium text-gray-500">Privelage Set</label>
+              <div class="mt-1 text-sm text-gray-900">
+                <EditableSelectCell
+                  v-model="userType"
+                  :is-editable="isEditable('type')"
+                  :options="userTypes"
+                  @blur="onBlur"
+                />
+              </div>
             </div>
             <div class="sm:col-span-1">
               <dt class="text-sm font-medium text-gray-500">Seconds in Day</dt>
-              <dd class="mt-1 text-sm text-gray-900">
-                <EditableInput v-model="secondsInDay" :is-editable="isSecondsInDayEditable" @blur="onBlur" />
+              <dd class="mt-1 text-sm text-gray-900" @click="toggleEditable('secondsInDay')">
+                <EditableInput v-model="secondsInDay" :is-editable="isEditable('secondsInDay')" @blur="onBlur" />
               </dd>
             </div>
           </dl>
           <dl class="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
             <div class="sm:col-span-1">
               <dt class="text-sm font-medium text-gray-500">Selectable</dt>
-              <dd class="mt-1 text-sm text-gray-900" @click="toggleEditable('type')">
+              <dd class="mt-1 text-sm text-gray-900">
                 <EditableCheckBoxCell v-model="selectable" />
               </dd>
             </div>
@@ -65,7 +70,7 @@
 </template>
 
 <script>
-import { events } from '~/shared/constants'
+import { events, userTypes } from '~/shared/constants'
 export default {
   name: 'UserDetails',
   props: {
@@ -144,17 +149,8 @@ export default {
         this.updateUser({ notifyOfLogins })
       },
     },
-    isTypeEditable() {
-      return this.editable === 'type'
-    },
-    isFirstNameEditable() {
-      return this.editable === 'firstName'
-    },
-    isLastNameEditable() {
-      return this.editable === 'lastName'
-    },
-    isSecondsInDayEditable() {
-      return this.editable === 'secondsInDay'
+    userTypes() {
+      return userTypes
     },
   },
   methods: {
@@ -164,9 +160,13 @@ export default {
     },
     toggleEditable(field) {
       this.editable = this.editable === field ? '' : field
+      console.log(this.editable)
     },
     onBlur() {
       this.toggleEditable('')
+    },
+    isEditable(field) {
+      return this.editable === field
     },
   },
 }
