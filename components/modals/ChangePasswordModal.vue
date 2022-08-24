7a20e9e5584ebaf65141106e3253b2d630edcc17
Yoshi Rubin <yoshrubin@gmail.com>
2022-08-24 14:07:21 +0300

    add loading indicator and set correct id type

--- components/modals/ChangePasswordModal.vue
+++ components/modals/ChangePasswordModal.vue
@@ -1,9 +1,9 @@
 <template>
-  <SubmitCard :disabled="!isPasswordValid" @hide="emitHide" @submit="submitPasswordChange">
+  <SubmitCard :loading="loading" :disabled="!isPasswordValid" @hide="emitHide" @submit="submitPasswordChange">
     <div class="w-full space-y-2">
       <span class="font-semibold">Change {{ user.username }}'s Password</span>
       <FormInput ref="input" v-model="newPassword" />
-      <FormInput ref="input" v-model="confirmPassword" />
+      <FormInput v-model="confirmPassword" />
     </div>
   </SubmitCard>
 </template>
@@ -16,14 +16,15 @@ export default {
   name: 'ChangePasswordModal',
   props: {
     userId: {
-      type: Object,
-      default: () => null,
+      type: Number,
+      required: true,
     },
   },
   data() {
     return {
       newPassword: '',
       confirmPassword: '',
+      loading: false,
     }
   },
   computed: {
@@ -32,7 +33,7 @@ export default {
       return Object.values(this.users).find((user) => user.id === this.userId)
     },
     isPasswordValid() {
-      return this.newPassword === this.confirmPassword && this.newPassword.length > 6
+      return this.newPassword === this.confirmPassword && this.newPassword.length >= 6
     },
   },
   mounted() {
@@ -42,7 +43,14 @@ export default {
     emitHide() {
       this.$emit(events.hide)
     },
-    submitPasswordChange() {},
+    async submitPasswordChange() {
+      this.loading = true
+      const headers = this.$api.getHeaders()
+      const userId = this.userId
+      const password = this.newPassword
+      await this.$api.updatePassword(headers, { userId }, { password })
+      this.loading = false
+    },
   },
 }
 </script>
