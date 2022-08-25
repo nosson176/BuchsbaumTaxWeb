67a1cd76d4254e26df578a95cdabf780b9ca40dd
Yoshi Rubin <yoshrubin@gmail.com>
2022-08-25 13:41:37 +0300

    map page layout

---
+++ components/maps-page/MapsView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="grid grid-cols-6 h-full">
+    <div class="col-span-1 col-start-6">
+      <ContactsList />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MapsView',
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style scoped></style>
