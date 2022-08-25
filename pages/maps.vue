eba92ee323781493b1daedc3bb3353a49de7224d
Yoshi Rubin <yoshrubin@gmail.com>
2022-08-25 13:41:27 +0300

    added map page

---
+++ pages/maps.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <Header />
+    <MapsView />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MapsPage',
+
+  data() {
+    return {}
+  },
+
+  mounted() {},
+
+  methods: {},
+}
+</script>
+
+<style scoped></style>
