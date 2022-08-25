76d7a0ed07c4c60991b2d589029e994707c4cdd1
Yoshi Rubin <yoshrubin@gmail.com>
2022-08-25 10:31:07 +0300

    simple iframe implementation for irs page

---
+++ pages/irs.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <iframe height="770" width="770" src="https://sa.www4.irs.gov/irfof/lang/en/irsessionexpired.jsp?app=IRFOF">
+    </iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IrsPage',
+
+  data() {
+    return {}
+  },
+
+  mounted() {},
+
+  methods: {},
+}
+</script>
+
+<style scoped></style>
