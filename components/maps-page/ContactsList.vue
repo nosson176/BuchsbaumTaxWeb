b368ac21b842cde9b1809abe488905d426556c6e
Yoshi Rubin <yoshrubin@gmail.com>
2022-08-28 11:45:51 +0300

    removed console

--- components/maps-page/ContactsList.vue
+++ components/maps-page/ContactsList.vue
@@ -84,7 +84,6 @@ export default {
       return contactType?.toLowerCase().includes('address')
     },
     isSelected({ id }) {
-      console.log(this.selectedContactId === id)
       return this.selectedContactId === id
     },
   },
