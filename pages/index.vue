de1f4714d69f3c5894d7a5041098f1c2c883be61
Yoshi Rubin <yoshrubin@gmail.com>
2022-08-30 15:42:20 +0300

    load data in mounted not created

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="min-h-screen flex flex-col">
     <Header />
-    <p v-if="$fetchState.pending">Fetching Data...</p>
-    <Home v-else />
+    <Home />
   </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
-  async fetch() {
-    await this.$api.getClientList(this.headers)
+  async asyncData({ $api }) {
+    const headers = $api.getHeaders()
+    await $api.getClientList(headers)
   },
   computed: {
     headers() {
       return this.$api.getHeaders()
     },
   },
-  async created() {
-    await this.$api.getValueTypes(this.headers)
-    await this.$api.getAllUsers(this.headers)
-    await this.$api.getValueTaxGroups(this.headers)
-    await this.$api.getAllClientFees(this.headers)
-    await this.$api.getClientsHistory(this.headers)
-    await this.$api.getSmartviews(this.headers)
+  mounted() {
+    this.$api.getValueTypes(this.headers)
+    this.$api.getAllUsers(this.headers)
+    this.$api.getValueTaxGroups(this.headers)
+    this.$api.getAllClientFees(this.headers)
+    this.$api.getClientsHistory(this.headers)
+    this.$api.getSmartviews(this.headers)
   },
 }
 </script>
