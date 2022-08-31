40d203bfe47f389994a3cfc75fc98790f0eaef0b
Yoshi Rubin <yoshrubin@gmail.com>
2022-08-31 15:31:45 +0300

    block loading if already loaded

--- pages/index.vue
+++ pages/index.vue
@@ -6,23 +6,38 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import { models } from '~/shared/constants'
+
 export default {
   name: 'IndexPage',
   async fetch() {
-    await this.$api.getClientList(this.headers)
+    if (!this.initLoaded) {
+      await this.$api.getClientList(this.headers)
+      await this.$api.getValueTypes(this.headers)
+      await this.$api.getAllUsers(this.headers)
+      await this.$api.getValueTaxGroups(this.headers)
+      await this.$api.getAllClientFees(this.headers)
+      await this.$api.getClientsHistory(this.headers)
+      await this.$api.getSmartviews(this.headers)
+    }
   },
   computed: {
+    ...mapState([models.loaded]),
     headers() {
       return this.$api.getHeaders()
     },
-  },
-  mounted() {
-    this.$api.getValueTypes(this.headers)
-    this.$api.getAllUsers(this.headers)
-    this.$api.getValueTaxGroups(this.headers)
-    this.$api.getAllClientFees(this.headers)
-    this.$api.getClientsHistory(this.headers)
-    this.$api.getSmartviews(this.headers)
+    initLoaded() {
+      return (
+        this.loaded[models.clients] &&
+        this.loaded[models.valueTypes] &&
+        this.loaded[models.users] &&
+        this.loaded[models.valueTaxGroups] &&
+        this.loaded[models.allClientFees] &&
+        this.loaded[models.clientsHistory] &&
+        this.loaded[models.smartviews]
+      )
+    },
   },
 }
 </script>
