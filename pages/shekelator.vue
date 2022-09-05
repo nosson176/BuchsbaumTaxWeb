3160755248376f2c14b46fbb0161195d7cab3377
Yoshi Rubin <yoshrubin@gmail.com>
2022-09-05 17:27:52 +0300

    add shekelator page

---
+++ pages/shekelator.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <Header />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShekelatorPage',
+
+  data() {
+    return {}
+  },
+
+  mounted() {},
+
+  methods: {},
+}
+</script>
+
+<style scoped></style>
