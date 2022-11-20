8c7c88d6503aebe36328dc3ba17d59c2eaedeab7
Yehoshua Levine <shuielevine@gmail.com>
2022-11-20 12:16:38 +0200

    Values page is admin only

--- components/values-page/Values.vue
+++ components/values-page/Values.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="w-full h-full flex flex-col">
-    <ValuesEditorSection />
+    <ValuesEditorSection v-if="isCurrentUserAdmin" />
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import { models, USER_TYPE_ADMIN } from '~/shared/constants'
+
 export default {
   name: 'Values',
+  computed: {
+    ...mapState([models.users, models.currentUser]),
+    isCurrentUserAdmin() {
+      return this.currentUser.userType === USER_TYPE_ADMIN
+    },
+  },
 }
 </script>
 
